8b41417eb28bd1c4a3ef4a4de6c0c97f0968acff
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-08-11 11:34:43 -0600

    se han agregado las cuatro opciones donde cada una tiene una funcion ademas de agregar el que no se pueda ingresar un dato si no hay agregado un numero

--- MenuUI.py
+++ MenuUI.py
@@ -1,7 +1,33 @@
-def agregar_Numero():
+def agregar_numero():
     print("===Registro de numeros===")
+    numero = int(input("Ingrese un numero: "))
+    numeros.append(numero)
+    print("Se ha agregado el numero exitosamente")
+
+def buscar_numero():
+    print("===Buscar numero===")
+    buscar_numeros = int(input("Ingrese un numero: "))
+    if buscar_numeros in numeros:
+        print(f"Se ha encontrado el numero {buscar_numeros} exitosamente")
+    else:
+        print(f"No se ha encontrado el numero {buscar_numeros}")
+
+def mostrar_numeros():
+    print("===Mostrar numeros====")
+    for numero in numeros:
+        print(numero)
+
+def eliminar_numero():
+    print("===Eliminar numero===")
+    numero = int(input("Ingrese un numero que desea eliminar: "))
+    if numero in numeros:
+        numeros.remove(numero)
+        print("Se ha eliminado el numero exitosamente")
+    else:
+        print(f"No se ha encontrado el numero {numero}")
 
 opcion =0
+numeros = []
 while opcion != 5:
     print("===MENU INTERACTIVO===")
     print("1. Agregar numeros")
@@ -17,5 +43,41 @@ while opcion != 5:
 
     match opcion:
         case 1:
-            agregar_Numero()
+            confirmacion = ""
+            while confirmacion !="no".lower():
+                agregar_numero()
+                print("Desea volver a ingresar un numero")
+                confirmacion = input("Escriba (si/no): ")
+
+        case 2:
+            if len(numeros) > 0:
+                confirmacion = ""
+                while confirmacion != "no".lower():
+                    buscar_numero()
+                    print("Desea volver a buscar un numero")
+                    confirmacion = input("Escriba (si/no): ")
+            else:
+                print("=== No hay numeros agregados ===")
+
+        case 3:
+            if len(numeros) > 0:
+                mostrar_numeros()
+            else:
+                print("=== No hay numeros agregados ===")
+
+        case 4:
+            if len(numeros) > 0:
+                confirmacion = ""
+                while confirmacion != "no".lower():
+                    eliminar_numero()
+                    print("Desea volver a eliminar un numero")
+                    confirmacion = input("Escriba (si/no): ")
+            else:
+                print("=== No hay numeros agregados ===")
+
+        case 5:
+            print("=== Saliendo del programa ===")
+
+
+
 
